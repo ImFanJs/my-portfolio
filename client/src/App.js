d7d2c1f2e929a4b6be9f923684a09860decff32f
jolmos <jolmos@advancedpricing.com>
2020-10-28 22:08:37 -0700

    Cleaning routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import Experience from './screens/Experience'
 import Education from './screens/Education'
 import Projects from './screens/Projects'
 import NotFound from './screens/NotFound'
-// import Contacts from './projects/contacts/Contacts'
 
 const App = () => {
   return (
@@ -19,7 +18,6 @@ const App = () => {
           <Route exact path="/experience" component={Experience} />
           <Route exact path="/education" component={Education} />
           <Route exact path="/projects" component={Projects} />
-          {/* <Route exact path="/projects/contacts" component={Contacts} /> */}
           <Route component={NotFound} />
         </Switch>
       </Router>
